b0e9d2d01b00970b2dc91474318d45b6319b400f
Lex <info@alexandranel.com>
2024-02-11 14:47:12 +1100

    have updated shapes, and svg.js. have made those two work together and am now working to pass prompts through to these from index.js

--- index.js
+++ index.js
@@ -1,61 +1,66 @@
 
 const fs = require('fs');
 const path = require('path');
-// const Shapes =  require ('./lib/shapes')
+const svgMaker = require('./lib/svg.js') //connecting the shapes module
 const inquirer = require('inquirer');
 // const logoPrompts = require('./lib/questions')
 
-async function logoPrompts(){
+async function logoPrompts() {
     //try block means I can use one .catch function for the entire block
-    try{
+    try {
 
-       const firstQ = await inquirer.prompt(
-        [{
-            type:'confirm',
-            name: 'start',
-            message:'Welcome! You have entered the logo generator, I will ask you a series of questions. A logo will be generated using your input. Press Y to start, press N to exit',
-        }]);
+        const firstQ = await inquirer.prompt(
+            [{
+                type: 'confirm',
+                name: 'start',
+                message: 'Welcome! You have entered the logo generator, I will ask you a series of questions. A logo will be generated using your input. Press Y to start, press N to exit',
+            }]);
 
         // checking response to see if user wants to proceed to this application, if not exit 
-    if (!firstQ.start){
-        console.log('exiting...');
-        process.exit();
-    }
-    
-    //async function continues on to the next Q if firstQ.start is true
-    //we continue on to ask all the required questions
+        if (!firstQ.start) {
+            console.log('exiting...');
+            process.exit();
+        }
 
-    const secondQ = await inquirer.prompt([
-        {
-            type: 'input',
-            name:'text',
-            message:'Choose up to 3 letters for your logo',
-        },
-        {
-            type: 'input',
-            name: 'textColour',
-            message: 'What colour would you like your logo TEXT to be? Write its name or the hex code',
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a shape that you would like your logo to be',
-            choices: ['Circle', 'Triangle', 'Square']
-        },
-        {
-            type: 'input',
-            name: 'logoColour',
-            message: 'What colour would you like your LOGO to be? Write its name or the hex code',
-        },
-    ]);
+        //async function continues on to the next Q if firstQ.start is true
+        //we continue on to ask all the required questions
 
-    console.log('Generated logo.svg, check your folder');
+        const secondQ = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'text',
+                message: 'Choose up to 3 letters for your logo',
+            },
+            {
+                type: 'input',
+                name: 'textColour',
+                message: 'What colour would you like your logo TEXT to be? Write its name or the hex code',
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'Choose a shape that you would like your logo to be',
+                choices: ['Circle', 'Triangle', 'Square']
+            },
+            {
+                type: 'input',
+                name: 'logoColour',
+                message: 'What colour would you like your LOGO to be? Write its name or the hex code',
+            },
+
+
+        ]);
+
+
+        console.log('Generated logo.svg, check your folder');
         //function to create logo here
 
     } catch (error) {
-        console.log ('This generator has malfunctioned, please try again', error);
-    
-}};
+        console.log('This generator has malfunctioned, please try again', error);
 
- 
+    }
+
+};
+
+
 logoPrompts();
